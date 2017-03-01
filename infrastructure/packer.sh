a47782c30ec7e2fe53c66a4b3910149974cfc0fe
Michael Wittig <post@michaelwittig.info>
2017-03-01 17:35:56 +0100

    fixed sed, added app copy

--- infrastructure/packer.sh
+++ infrastructure/packer.sh
@@ -4,5 +4,4 @@
 sudo yum -y update
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 sudo yum -y install nodejs
-# TODO get app and place into /opt/app
 sudo npm install -g forever@0.15.3
