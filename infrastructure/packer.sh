cfb1c56a74597b598eb8dfd012efdea01b809a6b
Michael Wittig <post@michaelwittig.info>
2017-03-01 19:41:08 +0100

    fixed input format, fixed root

--- infrastructure/packer.sh
+++ infrastructure/packer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # this script runs as ec2-user
 
-sudo yum -y update
-sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-sudo yum -y install nodejs
-sudo npm install -g forever@0.15.3
-sudo mv /tmp/app /opt
+sudo su
+yum -y update
+curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+yum -y install nodejs
+npm install -g forever@0.15.3
+mv /tmp/app /opt
