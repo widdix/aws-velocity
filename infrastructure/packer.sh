48ef04a75859b968fa0657baa7a3a4a22a920126
Michael Wittig <post@michaelwittig.info>
2017-03-02 08:44:53 +0100

    activate app and logs

--- infrastructure/packer.sh
+++ infrastructure/packer.sh
@@ -3,6 +3,7 @@
 
 sudo yum -y update
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
-sudo yum -y install nodejs
+sudo yum -y install nodejs awslogs
 sudo npm install -g forever@0.15.3
 sudo mv /tmp/app /opt
+
